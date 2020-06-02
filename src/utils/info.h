4acffd0ccf4f08ee0ae4c900ef4a08b8857c6db6
cheg <egerlanc@physik.hu-berlin.de>
2020-06-02 20:45:30 +0200

    Added header guards for info.h
    Signed-off-by: cheg <egerlanc@physik.hu-berlin.de>

--- src/utils/info.h
+++ src/utils/info.h
@@ -1,6 +1,10 @@
 #ifndef INFO_H
 #define INFO_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // from https://github.com/michael-hartmann/caps
 void print_spike_build_info(FILE *fp) {
 #ifdef GIT_HEAD
@@ -11,5 +15,9 @@ void print_spike_build_info(FILE *fp) {
   fprintf(fp, "# git branch: %s\n", GIT_BRANCH);
 #endif
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // INFO_H
